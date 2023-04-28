001605ba883b3905dcfc79761792569126bda7a2
parikshitgediya612 <131867270+parikshitgediya612@users.noreply.github.com>
2023-04-28 07:08:34 -0400

    Add files via upload

---
+++ MISSION3.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  clrscr();
+    int c,f;
+    printf("enter value c=");
+    scanf("%d",&c);
+    f=c*9/5+32;
+    printf("f=%d",f);
+    getch();
+}
---
+++ MISSION4.C
@@ -0,0 +1,19 @@
+   #include<stdio.h>
+   #include<conio.h>
+   main()
+   {
+       clrscr();
+       int bs,hra,da,ta;
+       printf("enter value of bs=");
+       scanf("%d",&bs);
+       printf("enter value of hra=");
+       scanf("%d",&hra);
+       printf("enter value of da=");
+       scanf("%d",&da);
+       printf("enter value of ta=");
+       scanf("%d",&ta);
+       printf("hra=bs*hra/100");
+       getch();
+   }
+
+
